603e8b032f88ca19eb2748459d1910ade2f2018f
Bairon Perez <baiper06@gmail.com>
2019-05-14 01:33:04 -0600

    Enable cors gem

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Author.create!([
+  { name: 'Author 001', email: 'Author@001'  },
+  { name: 'Author 002', email: 'Author@002'  },
+  { name: 'Author 003', email: 'Author@003'  },
+  { name: 'Author 004', email: 'Author@004'  }
+])
+
+
+Publication.create!([
+  { title: 'title 001', body: 'body@001', author_id: 1  },
+  { title: 'title 002', body: 'body@002', author_id: 1   },
+  { title: 'title 003', body: 'body@003', author_id: 1   },
+  { title: 'title 004', body: 'body@004', author_id: 2   }
+])
