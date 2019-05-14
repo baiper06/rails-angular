9eec3011699befbb2335b1a8d52ec5866d985423
Bairon Perez <baiper06@gmail.com>
2019-05-13 22:26:24 -0600

    Create Angular providers and services

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,15 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+/*Other imports*/
+import { ApiService } from './api.service';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthorAddComponent,
+    AuthorListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
