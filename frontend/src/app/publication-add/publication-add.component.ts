fa77795662298e91366e99dd10cf408169a8f79b
Bairon Perez <baiper06@gmail.com>
2019-05-14 00:55:55 -0600

    Implement CRUD Methods for Publications

--- frontend/src/app/publication-add/publication-add.component.ts
+++ frontend/src/app/publication-add/publication-add.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Publication } from '../publication';
+import { ApiService } from '../api.service';
+import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-publication-add',
@@ -7,9 +11,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PublicationAddComponent implements OnInit {
 
-  constructor() { }
+  public publication : Publication  = new Publication();
+
+  constructor(public apiService: ApiService , public acRoute : ActivatedRoute) { }
 
   ngOnInit() {
+    this.acRoute.params.subscribe((data : any)=>{
+    console.log(data.id);
+    if(data && data.id){
+        this.apiService.get("publications/"+data.id).subscribe((data : Publication)=>{
+        this.publication = data;
+        });
+    }
+    else
+    {
+        this.publication = new Publication();
+    }
+    })
   }
 
+
+  public onSubmit(){
+    console.log("Adding a publication: " + this.publication.title);
+    if(this.publication.id){
+    this.apiService.update("publications/"+this.publication.id,this.publication).subscribe((r)=>{
+        console.log(r);
+        alert("Publication updated !");
+    })
+    }
+    else
+    this.apiService.post("publications",this.publication).subscribe((r)=>{
+    console.log(r);
+    this.publication = new Publication();
+    alert("Publication added !");
+
+    });
+  }
+
+
 }
+
+
+
+
+
+
+
+
--- frontend/src/app/publication-list/publication-list.component.ts
+++ frontend/src/app/publication-list/publication-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiService } from '../api.service';
+import { Publication } from '../publication';
+
 
 @Component({
   selector: 'app-publication-list',
@@ -7,9 +11,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PublicationListComponent implements OnInit {
 
-  constructor() { }
+  public attributes = ['id', 'author', 'title', 'body', 'date', 'time'];
+  public publications : Array<Publication>; 
+
+  constructor(public apiService: ApiService , public router : Router) {
+  }
 
   ngOnInit() {
+    this.apiService.get("publications").subscribe((data : Publication[])=>{
+    console.log(data);
+    this.publications = data;
+    });
   }
+
+
+  public delete(id:string){
+
+    console.log("delete : " + id);
+    var path = 'publications/' + id;
+    this.apiService.delete(path).subscribe((r)=>{
+
+    this.publications = this.publications.filter((p,i)=>{
+
+        if(Number(id) === p.id ) 
+        {
+        return false;
+        }
+        return true;
+    },this.publications)
+
+    });
+
+  }
+
+  public update(id:string){
+    console.log("update : " + id );
+    this.router.navigateByUrl('/publications/add/' + id);
+  }
+
+
+  public create(){
+    console.log("create" );
+    this.router.navigateByUrl('/publications/add/');
+  }
+
 
 }
