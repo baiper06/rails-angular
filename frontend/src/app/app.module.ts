2362306a19ddca902c638aee1d49502707912b80
Bairon Perez <baiper06@gmail.com>
2019-05-13 22:33:53 -0600

    Add Authors routing

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,6 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { RouterModule } from '@angular/router';
+import { AuthorAddComponent } from './author-add/author-add.component';
+import { AuthorListComponent } from './author-list/author-list.component';
+
 /*Other imports*/
 import { ApiService } from './api.service';
 
@@ -15,10 +19,23 @@ import { ApiService } from './api.service';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+	RouterModule.forRoot([
+		  {
+		    path: 'authors',
+		    component: AuthorListComponent
+		  },
+		  {
+		    path: 'authors/add',
+		    component: AuthorAddComponent
+		  },
+		  {
+		    path: 'authors/add/:id',
+		    component: AuthorAddComponent
+		  }    
+		])
   ],
   providers: [ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-
