3579a0279ca0129d4ded07f236b2af3560c95a3d
Bairon Perez <baiper06@gmail.com>
2019-05-14 00:15:08 -0600

    Create Publication components

---
+++ frontend/src/app/publication-add/publication-add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-publication-add',
+  templateUrl: './publication-add.component.html',
+  styleUrls: ['./publication-add.component.sass']
+})
+export class PublicationAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
